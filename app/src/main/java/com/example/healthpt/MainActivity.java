2ad66bf6dfbd046cca62b538387ed2adfd3ed5ec
adoveflash <adoveflash@naver.com>
2025-05-04 16:07:06 +0900

    테스팅

--- app/src/main/java/com/example/healthpt/MainActivity.java
+++ app/src/main/java/com/example/healthpt/MainActivity.java
@@ -2,49 +2,129 @@ package com.example.healthpt;
 
 import android.os.Bundle;
 
-import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 import com.example.healthpt.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+//TODO 25/05/04 장윤상
+//1. 날려먹은거 복구하기 (난 너를 저주한다 맥북)
+//2. 깃에 수정된거 푸쉬 및 커밋하기
+//3. 홈화면 최대한 많이 구현해놓기
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
 
-        setSupportActionBar(binding.toolbar);
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        //binding = ActivityMainBinding.inflate(getLayoutInflater());
+        //setContentView(binding.getRoot());
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
-                        .setAction("Action", null).show();
+        if (savedInstanceState == null) {
+            loadFragment(new HomeFragment()); // 또는 CalendarFragment()
+        }
+
+        bottomNav.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+
+            if (item.getItemId() == R.id.nav_calendar) {
+                selectedFragment = new CalendarFragment();
             }
+            else if (item.getItemId() == R.id.nav_home) {
+                selectedFragment = new HomeFragment();
+            }
+            else if (item.getItemId() == R.id.nav_work){
+                selectedFragment = new WorkFragment();
+            }
+
+            else if(item.getItemId() == R.id.nav_community){
+                selectedFragment = new CommunityFragment();
+            }
+
+            if (selectedFragment != null) {
+                loadFragment(selectedFragment);
+                return true;
+            }
+            return false;
         });
+
     }
+
+    // 화면 전환
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
+    }
+
+    //현재 인원 설정
+
+
+
+    /*
+    void loadFragment(HomeFragment());
+
+
+    bottomNav.setOnItemSelectedListener(item -> {
+        Fragment selectedFragment = null;
+
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                selectedFragment = new HomeFragment();
+                // 홈 화면 이동
+                break;
+            case R.id.nav_work:
+                selectedFragment = new WorkFragment();
+                // 운동 화면 이동
+                break;
+            case R.id.nav_calendar:
+                selectedFragment = new CalendarFragment();
+                // 캘린더 이동
+                break;
+            case R.id.nav_community:
+                selectedFragment = new CommunityFragment();
+                // 커뮤니티 이동
+                break;
+        }
+        if (selectedFragment != null) {
+            loadFragment(selectedFragment);
+            return true;
+        }
+
+        return false;
+    });
+
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
+    }
+
+
+     */
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -68,10 +148,5 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
+
 }
